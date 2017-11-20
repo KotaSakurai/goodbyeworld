9f94cecf848a3ac90f9783e3c9996480668284d2
Daisukeengneer <32788232+Daisukeengneer@users.noreply.github.com>
2017-11-20 18:56:48 +0900

    judgeを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
   root 'images#index'
-  resources :images
+  resources : images
+  resources : judges,only:[:index ,:create]
 end
