0bf2c27db385124768b1c54dccf26b0196a116b6
kunieda yuki <yuki@kunie-no-MacBookAir11.local>
2017-11-21 11:54:53 +0900

    ジャッジボタン修正

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,7 @@
 class Image < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_many :judges
+
+  has_many :vote_to_male, -> {where(gender:0)}, class_name:"Judge"
+  has_many :vote_to_female, -> {where(gender:1)}, class_name:"Judge"
 end
