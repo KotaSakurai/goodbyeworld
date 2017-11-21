371368c93f3c9f1fc55cf1dc147168f358f0245a
timetam1 <tanuma321bre@gmail.com>
2017-11-21 10:43:51 +0900

    judgeコントローラの追加(2回目)

--- app/controllers/judges_controller.rb
+++ app/controllers/judges_controller.rb
@@ -1,20 +1,18 @@
 class JudgesController < ApplicationController
-	def index
-		@judge = Judge.new
-	end
+  def index
+    @judge = Judge.new
+  end
 
-    def create
-    	@judge = Judge.new(creates_params)
-    	if @judge.save
-    	 redirect_to root_path
-        end
-    end
+  def create
+    @judge = Judge.new(judge_params)
+    if @judge.save
+    redirect_to root_path
+  end
+  end
 
-    private
-    def creates_params
-    	params.require(:judge).permit(:images_id, :gender)
-    end
-
-
+  private
+  def judge_params
+    params.require(:judge).permit(:image_id, :gender)
+  end
 
 end
